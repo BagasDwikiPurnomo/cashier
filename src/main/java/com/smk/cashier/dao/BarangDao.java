1588bc2fa7a79beadafcec69b9fd1bc1b7349522
BagasDwikiPurnomo <bgstopia@gmail.com>
2023-10-25 21:29:06 +0700

    Create Barang Update

--- src/main/java/com/smk/cashier/dao/BarangDao.java
+++ src/main/java/com/smk/cashier/dao/BarangDao.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
 
-public class BarangDao implements Dao<Barang, Integer>{
+public class BarangDao implements Dao<Barang, Integer> {
     private final Optional<Connection> connection;
 
     public BarangDao() {
@@ -23,7 +23,7 @@ public class BarangDao implements Dao<Barang, Integer>{
                 PreparedStatement ps = conn.prepareStatement(sql);
                 ps.setInt(1, id);
                 ResultSet rs = ps.executeQuery();
-                if(rs.next()) {
+                if (rs.next()) {
                     String kodeBarang = rs.getString("kode_barang");
                     String namaBarang = rs.getString("nama_barang");
                     int hargaBarang = rs.getInt("harga_barang");
@@ -35,7 +35,7 @@ public class BarangDao implements Dao<Barang, Integer>{
                     barang = Optional.of(barangResult);
                 }
             } catch (SQLException e) {
-               e.printStackTrace();
+                e.printStackTrace();
             }
             return barang;
 
@@ -53,26 +53,26 @@ public class BarangDao implements Dao<Barang, Integer>{
         Barang nonNullBarang = Objects.requireNonNull(barang);
         String sql = "INSERT INTO Barang + (kode_barang, nama_barang, harga_barang, created_by, updated_by, date_created, last_modified) VALUES(?,?,?,?,?,?,?)";
 
-        return  connection.flatMap(conn -> {
+        return connection.flatMap(conn -> {
             Optional<Integer> generatedId = Optional.empty();
             try {
 
                 PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-                ps.setString(1,barang.getKodeBarang());
-                ps.setString(2,barang.getNamaBarang());
-                ps.setInt(3,barang.getHargaBarang());
+                ps.setString(1, barang.getKodeBarang());
+                ps.setString(2, barang.getNamaBarang());
+                ps.setInt(3, barang.getHargaBarang());
                 ps.setString(4, barang.getCreatedBy());
                 ps.setString(5, barang.getUpdatedBy());
-                ps.setDate(6,new Date(barang.getDateCreated().getTime()));
-                ps.setDate(7,new Date(barang.getLastModified().getTime()));
+                ps.setDate(6, new Date(barang.getDateCreated().getTime()));
+                ps.setDate(7, new Date(barang.getLastModified().getTime()));
 
                 int numberofInsertedRows = ps.executeUpdate();
 
-                if (numberofInsertedRows > 0){
+                if (numberofInsertedRows > 0) {
                     ResultSet rs = ps.getGeneratedKeys();
 
-                if (rs.next())
-                    generatedId = Optional.of(rs.getInt(1));
+                    if (rs.next())
+                        generatedId = Optional.of(rs.getInt(1));
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -85,10 +85,24 @@ public class BarangDao implements Dao<Barang, Integer>{
     @Override
     public void update(Barang barang) {
 
+        Barang nonNullBarang = Objects.requireNonNull(barang);
+        String sql = "UPDATE barang SET harga_barang = ?, nama_barang = ?, last_modified = ?, updated_by = ? WHERE kode_barang = ?";
+        connection.ifPresent(conn -> {
+            try {
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ps.setInt(1, barang.getHargaBarang());
+                ps.setString(2, barang.getNamaBarang());
+                ps.setDate(3, new Date(new java.util.Date().getTime()));
+                ps.setString(4, "Kimihiko");
+                ps.setString(5, barang.getKodeBarang());
+                int numberOfUpdatedRows = ps.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
     public void delete(Barang barang) {
-
     }
 }
